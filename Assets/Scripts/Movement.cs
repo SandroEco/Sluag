e9caac45e8a2bb5442300dc96d6a40df5c1fb3c5
SandroEco <sandro.ecora@htk.academy>
2022-03-20 21:31:34 +0100

    Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public static Movement Instance { get; private set; }
-
-    public float speed = 5f;
-    public float jumpForce = 5f;
-
-    private Rigidbody2D rb;
-
     public SceneDetails CurrentScene { get; private set; }
     public SceneDetails PrevScene { get; private set; }
+
+    [Header("Components")]
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+    private Animator anim;
+
+    [Header("Layer Masks")]
+    [SerializeField]private LayerMask groundLayer;
+
+    [Header("Movement Variables")]
+    public float movementAcceleration;
+    public float maxMoveSpeed;
+    public float movementDeceleration;
+    private float horizontalDirection;
+    private bool changingDirection => (rb.velocity.x > 0f && horizontalDirection < 0f) || (rb.velocity.x < 0f && horizontalDirection > 0f);
+    private bool isFacingRight = true;
+       
+    [Header("Jump Variables")]
+    public float jumpForce = 12f;
+    public float airLinearDrag = 2.5f;
+    public float fallMultiplier = 8f;
+    public float lowJumpFallMultiplier = 5f;
+    private bool canJump => Input.GetButtonDown("Jump") && isGrounded;
+
+    [Header("Ground Collision Variables")]
+    public float groundRaycastLength;
+    public bool isGrounded;
 
     private void Awake()
     {
@@ -22,26 +43,117 @@ public class Movement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         #region Movement
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+        horizontalDirection = GetInput().x;
         #endregion
 
         #region Jumping
-        if(Input.GetKeyDown("space") && Mathf.Abs(rb.velocity.y) < 0.001f)
+        if (canJump) Jump();
+        if (isGrounded)
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            ApplyGroundLinearDrag();
+        }
+        else
+        {
+            ApplyAirLinearDrag();
+            FallMultiplier();
         }
         #endregion
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+        CheckCollisions();
     }
 
     public void SetCurrentScene(SceneDetails currScene)
     {
         PrevScene = CurrentScene;
         CurrentScene = currScene;
+    }
+
+    private Vector2 GetInput()
+    {
+        return new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+    private void MoveCharacter()
+    {
+        rb.AddForce(new Vector2(horizontalDirection, 0f) * movementAcceleration);
+        anim.SetFloat("speed", Mathf.Abs(horizontalDirection));
+
+        if(isFacingRight && horizontalDirection < 0 || !isFacingRight && horizontalDirection > 0)
+        {
+            Flip();
+        }
+
+        if(Mathf.Abs(rb.velocity.x) > maxMoveSpeed)
+        {
+            anim.SetBool("MaxSpeedReached", true);
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxMoveSpeed, rb.velocity.y);
+        }
+        else
+        {
+            anim.SetBool("MaxSpeedReached", false);
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+    }
+
+    private void ApplyAirLinearDrag()
+    {
+        rb.drag = airLinearDrag;
+    }
+
+    private void ApplyGroundLinearDrag()
+    {
+        if(Mathf.Abs(horizontalDirection) < 0.4f || changingDirection)
+        {
+            rb.drag = movementDeceleration;
+        }
+        else
+        {
+            rb.drag = 0f;
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void FallMultiplier()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallMultiplier;
+        }
+        else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.gravityScale = lowJumpFallMultiplier;
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+    }
+
+    private void CheckCollisions()
+    {
+        isGrounded = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, groundRaycastLength, Vector2.down, groundRaycastLength, groundLayer);
+        Debug.DrawRay(bc.bounds.center + new Vector3(bc.bounds.extents.x, 0), Vector2.down * (bc.bounds.extents.y + groundRaycastLength), Color.red);
+        Debug.DrawRay(bc.bounds.center - new Vector3(bc.bounds.extents.x, 0), Vector2.down * (bc.bounds.extents.y + groundRaycastLength), Color.red);
+        Debug.DrawRay(bc.bounds.center - new Vector3(bc.bounds.extents.x, bc.bounds.extents.y + 0.1f), Vector2.right * (bc.bounds.extents.x + groundRaycastLength), Color.red);
     }
 }
