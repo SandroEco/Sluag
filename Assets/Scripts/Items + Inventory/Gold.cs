aa93051a55b768af67669620a62df2495e40f81d
SandroEco <sandro.ecora@htk.academy>
2022-04-03 13:12:32 +0200

    Gold Chest

--- Assets/Scripts/Items + Inventory/Gold.cs
+++ Assets/Scripts/Items + Inventory/Gold.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Gold : MonoBehaviour
 {
     public InventoryScript IS;
+    private BoxCollider2D bc;
 
     public int goldValue;
     private void Start()
     {
         IS = FindObjectOfType<InventoryScript>();
+        bc = GetComponent<BoxCollider2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -18,6 +20,15 @@ public class Gold : MonoBehaviour
         {
             IS.gold = IS.gold + goldValue;
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            IS.gold = IS.gold + goldValue;
+            Destroy(bc);
         }
     }
 }
