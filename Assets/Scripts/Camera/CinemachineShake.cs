0613e127e7a2611298902223f730a06b9d9e72e2
SandroEco <sandro.ecora@htk.academy>
2022-05-01 12:54:26 +0200

    Screen Shake

---
+++ Assets/Scripts/Camera/CinemachineShake.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineShake : MonoBehaviour
+{
+    public static CinemachineShake ShakeInstance { get; private set; }
+
+    private CinemachineVirtualCamera cvc;
+    private float shakeTimer;
+
+    private void Awake()
+    {
+        ShakeInstance = this;
+        cvc = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = cvc.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        shakeTimer = time;
+    }
+
+    private void Update()
+    {
+        if(shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if(shakeTimer <= 0)
+            {
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = cvc.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+            }
+        }
+    }
+}
--- Assets/Scripts/Movement and Stuff/CombatRelated.cs
+++ Assets/Scripts/Movement and Stuff/CombatRelated.cs
@@ -53,6 +53,7 @@ public class CombatRelated : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         if (other.tag == "BearAntlers")
         {
+            CinemachineShake.ShakeInstance.ShakeCamera(4f, .1f);
             isKnockbacked = true;
             Vector2 difference = (transform.position - other.transform.position);
             difference.y = 1f;
@@ -68,6 +69,7 @@ public class CombatRelated : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         if (other.gameObject.tag == "BearBody")
         {
+            CinemachineShake.ShakeInstance.ShakeCamera(4f, .1f);
             isKnockbacked = true;
             Vector2 difference = (transform.position - other.transform.position);
             difference.y = 0.4f;
